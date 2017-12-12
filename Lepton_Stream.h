ac9dd94554dc89cb53c9bd347950e3dfb24b954a
KenthJohan <kenth.johan.work@gmail.com>
2017-12-12 16:16:00 +0000

    test

--- Lepton_Stream.h
+++ Lepton_Stream.h
@@ -4,7 +4,11 @@
 //Lepton_Packet_Is_Discard
 //Lepton_Packet_Is_Row
 //Lepton_Packet_Is_Match
+#include "Lepton.h"
 #include "Lepton_Packets.h"
+#include "Lepton_SPI.h"
+#include "Lepton_Pixels.h"
+#include "Lepton_Conversions.h"
 
 //Using:
 //size_t
@@ -41,3 +45,35 @@ int Lepton_Stream_Validity
    }
    return 1;
 }
+
+
+
+
+#define Lepton_Stream_Invalid_Segment   (0)
+#define Lepton_Stream_Invalid           (-1)
+#define Lepton_Stream_Shifting          (-2)
+#define Lepton_Stream_SPI_Error         (-3)
+
+int Lepton_Stream_Accept (int Device, struct Lepton_Pixel_Grayscale16 * Pixmap)
+{
+   struct Lepton_Packet Packet [Lepton_Height];
+   int Success;
+   int Segment;
+   Success = Lepton_SPI_Transfer_Stream (Packet, Lepton_Height, Device);
+   if (!Success) {return Lepton_Stream_SPI_Error;}
+   Success = Lepton_Stream_Validity (Packet, Lepton_Height);
+   if (!Success) {return Lepton_Stream_Invalid;}
+   if (Packet [20].Number != 20)
+   {
+      Lepton_SPI_Transfer_Stream (Packet, 1, Device);
+      return Lepton_Stream_Shifting;
+   }
+   Segment = Packet [20].Reserved >> 4;
+   if ((Segment <= 0) || (Segment > 4)) {return Lepton_Stream_Invalid_Segment;}
+   for (size_t I = 0; I < Lepton_Height; I = I + 1)
+   {
+      Lepton_Conversions_Packet_To_Grayscale16 (Packet + I, Pixmap, Segment - 1);
+   }
+   return Segment;
+}
+
